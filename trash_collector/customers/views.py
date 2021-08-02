1108ee933569dbec315d4549cd9baaf077d08b0f
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-02 15:48:31 -0500

    fixed customer index.html so index can reference customer/user info when logged in

--- trash_collector/customers/views.py
+++ trash_collector/customers/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Customer
+
 # Create your views here.
 
 # TODO: Create a function for each path created in customers/urls.py. Each will need a template as well.
@@ -9,10 +10,15 @@ from .models import Customer
 def index(request):
     # The following line will get the logged-in in user (if there is one) within any view function
     user = request.user
+    context = {
+        'user': user
+    }
 
     try:
         # This line inside the 'try' will return the customer record of the logged-in user if one exists
         logged_in_customer = Customer.objects.get(user=user)
+        context ['logged_in_customer'] = logged_in_customer
+
     except:
         # TODO: Redirect the user to a 'create' function to finish the registration process if no customer record found
         pass
@@ -20,4 +26,4 @@ def index(request):
     # It will be necessary while creating a Customer/Employee to assign request.user as the user foreign key
 
     print(user)
-    return render(request, 'customers/index.html')
+    return render(request, 'customers/index.html', context)
