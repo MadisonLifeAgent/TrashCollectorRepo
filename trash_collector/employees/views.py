f74760eb64323a98d68ba3664aa5793b46e7a8b3
Ryan <ryan@ryannd.com>
2021-08-03 14:47:43 -0500

    Added view and pages for employees to register new pickups

--- trash_collector/employees/views.py
+++ trash_collector/employees/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponseRedirect
 from customers.models import Customer
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
 from django.apps import apps
 from .models import Employee
@@ -18,6 +20,69 @@ def index(request):
     # Get the current user and the associated employee object
     current_user = request.user
     current_employee = Employee.objects.get(user = current_user)
+
+    todays_customers = get_todays_customers(current_employee)
+
+    context = {
+        'user': current_user,
+        'todays_customers': todays_customers
+    }
+
+    return render(request, 'employees/index.html', context)
+
+
+
+
+
+
+def register_pickup(request):
+    # This line will get the Customer model from the other app, it can now be used to query the db for Customers
+    Customer = apps.get_model('customers', 'Customer')
+    CompletedPickup = apps.get_model('customers', 'CompletedPickup')
+
+    # Get the current user and the associated employee object
+    current_user = request.user
+    current_employee = Employee.objects.get(user = current_user)
+
+    customers = get_todays_customers(current_employee)
+
+    if request.method == "POST":
+        # Get selected customers
+        selected_customer_ids = request.POST.getlist("customer_checks")
+        today = datetime.date.today()
+
+        # Make a new CompletedPickup for each selected customer
+        for customer_id in selected_customer_ids:
+            current_customer = Customer.objects.get(id=customer_id)
+            current_pickup = CompletedPickup(date=today, customer=current_customer, employee=current_employee)
+            current_pickup.save()
+
+        return HttpResponseRedirect(reverse("employees:index"))
+    else:
+        eligible_customers = get_eligible_pickup_customers(customers)
+
+        context = {
+            "user" : current_user,
+            "eligible_customers": eligible_customers
+        }
+        return render(request, 'employees/register_pickup.html', context)
+
+def get_eligible_pickup_customers(todays_customers):
+    today = datetime.date.today()
+    eligible_pickup_customers = []
+    CompletedPickup = apps.get_model('customers', 'CompletedPickup')
+
+    todays_completed_pickups = CompletedPickup.objects.filter(date=today)
+    ineligible_ids = [pickup.customer_id for pickup in todays_completed_pickups]
+
+    eligible_pickup_customers = [customer for customer in todays_customers if customer.id not in ineligible_ids]
+
+    return eligible_pickup_customers
+
+
+def get_todays_customers(current_employee):
+    Special_pickups = apps.get_model('customers', 'Special_pickups')
+    todays_customers = []
 
     # Gets the day of the week as an index. 0=Monday, 1=Tuesday, etc.
     today = datetime.date.today()
@@ -38,12 +103,8 @@ def index(request):
     # Merge the lists to make sure only unique customers are included
     todays_customers = merge_unique_customers(todays_customers, active_special_pickup_customers)
 
-    context = {
-        'user': current_user,
-        'todays_customers': todays_customers
-    }
+    return todays_customers
 
-    return render(request, 'employees/index.html', context)
 
 def get_weekday_string(day):
     day_num = day.weekday()
