c2c3210d8dde15738104c5ecf262e9c2c429f600
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-03 14:25:39 -0500

    added static files path to settings.py

--- trash_collector/trash_collector/settings.py
+++ trash_collector/trash_collector/settings.py
@@ -111,7 +111,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR, 'static',
+    '/trash_collector_starter/trash_collector_starter/trash_collector/static'
 ]
 
 # Default primary key field type
