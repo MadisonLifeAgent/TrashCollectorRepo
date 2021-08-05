c6bbe68e0396ae02fb85bd6c568c6e77698ea7f7
Ryan <ryan@ryannd.com>
2021-08-05 16:10:37 -0500

    Added customer detail page to employee site, not fully linked yet

--- trash_collector/employees/urls.py
+++ trash_collector/employees/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path("create/", views.create, name="create"),
     path("register_pickup/", views.register_pickup, name="register_pickup"),
     path("search_completed_pickups/", views.search_completed_pickups, name="search_completed_pickups"),
-    path("customers_by_day/", views.customers_by_day, name="customers_by_day")
+    path("customers_by_day/", views.customers_by_day, name="customers_by_day"),
+    path("customer_detail/<int:customer_id>/", views.customer_detail, name="customer_detail")
 ]
